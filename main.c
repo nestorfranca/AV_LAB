810362298856d7fdb5f5ff68e6fcca73ae244c67
gustavo-a-mendes <gust4v0.a.mendes@gmail.com>
2024-03-31 06:06:06 -0300

    :broom: cleanup: Eliminando códigos comentados

--- main.c
+++ main.c
@@ -8,40 +8,10 @@ int main(void) {
     inicializaTabela(contatos);
     importaContato(contatos);
     
-
-    imprimeCatalogo(contatos, 1);
-    // int var;
-    // do {
-    //     var = menu_principal();
-    // } while (var != '5');
     int var;
     do {
         var = menu_principal(contatos);
     } while (var != '5');
-    
-    // preencheContato(contatos);
 
-    // int i;
-    // for (i = 0; i < TAM; i++)
-    // {
-    //     printf("NOME: %s\n", contatos[i].nome);
-    //     printf("NOME: %s\n", contatos[i].nome);
-    //     printf("TEL: %s\n", contatos[i].telefone);
-    //     printf("EMAIL: %s\n\n", contatos[i].email);
-    // }
-   /* 
-    Contato *contato_encontrado = buscar_contatos(contatos, nome, telefone, email);
-
-    if (contato_encontrado != NULL) {
-        printf("Contato encontrado: %s\n", contato_encontrado->nome);
-        printf("TEL: %s\n", contato_encontrado->telefone);
-        printf("EMAIL: %s\n", contato_encontrado->email);
-    } else {
-        printf("Contato não encontrado.\n");
-    }
- */   
-    // Contato teste_1 = ;
-
-    // printf("%d", contadorBit(4096));
     return 0;
 }
